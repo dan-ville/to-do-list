74f2334bb2982a14413eb54f26e5eeeb1d06f5b7
dan-ville <d.villegas@visiontravel.net>
2020-11-13 11:37:46 -0500

    first commit

---
+++ todo.js
@@ -0,0 +1,17 @@
+const todo = {
+    personal: {
+        urgent: 'pay back everyone through zelle'
+        1: "work out",
+        2: "make banana bread",
+        3: "read the bible",
+        4: "read dave ramsey book",
+        5: "catch up with vanilla spice",
+        6: "finish character profile for naila, prilla, and raiza"
+    }
+    work: {
+        1: "research photography lighting",
+        2: "edit photos",
+        3: "research videography mics--ask santi",
+        4: 
+    }
+}
